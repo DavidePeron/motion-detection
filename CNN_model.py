86ff01e72007cb2988dc368fbfe2b10f6480a0f2
Marta <marta.carraro.5@studenti.unipd.it>
2018-06-07 15:39:53 +0200

    Few lines of CNN

---
+++ CNN_model.py
@@ -0,0 +1,20 @@
+import numby as np
+from keras import layers
+from keras import
+from keras import
+from keras import
+
+
+
+
+
+ X_train_orig, Y_trian_orig, X_test_orig, Y_test_orig = load_dataset()
+
+ #should I normalize the vectors?
+
+
+ print('number of training examples = ')
+
+
+
+ def model
